bf45d4b576a61966d851e861e4f92229c6f065d6
HarshitRamSingh <hsing127@asu.edu>
2024-12-06 22:44:03 -0700

    Fixing CORS Issue - 2

--- src/components/IntegratedQueryBuilder.tsx
+++ src/components/IntegratedQueryBuilder.tsx
@@ -312,12 +312,8 @@ WHERE {
     try {
       setIsLoading(true);
       const response = await axios.get(
-        `https://74.179.61.231:7200/repositories/ser531new`,
+        `http://74.179.61.231:7200/repositories/ser531new`,
         {
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-          },
           params: {
             query: query.trim(),
           },
@@ -349,7 +345,7 @@ WHERE {
     try {
       setIsLoading(true);
       const response = await axios.get(
-        `https://74.179.61.231:7200/repositories/ser531new`,
+        `http://74.179.61.231:7200/repositories/ser531new`,
         {
           params: {
             query: generatedMortalityQuery.trim(),
